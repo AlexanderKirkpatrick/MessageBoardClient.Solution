ef5d383017ff4de0e294cef085045a44c17a9b76
Alexander <knibz@protonmail.com>
2022-08-31 23:36:18 -0400

    add controllers with routes and CRUD capability

---
+++ MessageBoardClient/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MessageBoardClient.Models;
+
+namespace MessageBoardClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
