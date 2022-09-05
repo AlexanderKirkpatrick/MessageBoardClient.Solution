62b86b12409cbd397da54d0ee03db43ae8cb53be
Alexander <knibz@protonmail.com>
2022-09-04 20:06:56 -0400

    heavily scale back project, reducing number of models and controllers to one

--- MessageBoardClient/Program.cs
+++ MessageBoardClient/Program.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace MessageBoardClient
 {
